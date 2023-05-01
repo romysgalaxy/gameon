8f3f60e2b4f20475a00179c961d300df1a931d18
Romy <romane.pld@gmail.com>
2023-05-01 10:58:47 +0200

    fix responsive modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,9 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const modalbgConfirm = document.querySelector(".bground-confirm");
 const modalBtnConfirm = document.querySelectorAll(".modal-btn-confirm");
 const formData = document.querySelectorAll(".formData");
+const header = document.querySelector('header#myTopnav')
+const section = document.querySelector('section.hero-section')
+const footer = document.querySelector('footer')
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,10 +23,20 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  if (window.screen.width > 799) {
+      section.style.display = 'none';
+      header.style.display = 'none';
+      footer.style.display = 'none';
+  }
 }
 
 function closeModal() {
   modalbg.style.display = "none"; // Cacher la fenêtre modale
+  if (window.screen.width > 799) {
+    section.style.display = 'flex';
+    header.style.display = 'flex';
+    footer.style.display = 'block';
+}
 }
 
 function closeModalConfirm() {
